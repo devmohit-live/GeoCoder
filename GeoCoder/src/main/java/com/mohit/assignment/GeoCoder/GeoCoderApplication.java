6a6ade8f41b377a39c2897e5a4a75884483583a3
Mohit <mohitsingh.it5@gmail.com>
2022-09-23 15:05:49 +0530

    Setted up Basic Structure | Mohit

---
+++ GeoCoder/src/main/java/com/mohit/assignment/GeoCoder/GeoCoderApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.assignment.GeoCoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoCoderApplication.class, args);
+	}
+
+}
---
+++ GeoCoder/src/test/java/com/mohit/assignment/GeoCoder/GeoCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.assignment.GeoCoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
